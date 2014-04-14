b2236dcff62f0deb949d92460dad0d457b9b5553
Andy Hsu <rastetle13@gmail.com>
2014-04-14 08:05:21 +0000

    basic structure

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
-# encoding: utf-8
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 
-def index(request):
-	return render_to_response('index.html')
+def home(request):
+        return render_to_response('index.html')
+
